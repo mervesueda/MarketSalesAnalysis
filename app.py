a2c8b17c48428465d9f08879573b06be79dc4ee0
Merve Sueda Aydın <mervesueda2005@gmail.com>
2025-08-18 14:51:37 +0300

    app.py added

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from prophet import Prophet
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from sklearn.metrics import mean_squared_error, r2_score
+
+#Sayfa Başlığı ve Açıklama
+st.set_page_config(page_title="Market Sales Analysis", layout="wide")
+
+st.title("📊 Market Sales Analysis Dashboard")
+st.markdown("""
+Bu uygulama, satış verilerini analiz etmek, linear regression modeli ve **zaman serisi tahminleri** (SARIMA & Prophet) gerçekleştirmek için geliştirilmiştir.
+""")
+
+df = pd.read_csv("train.csv")   # buradaki ismi senin dosyaya göre değiştir
+df["Order Date"] = pd.to_datetime(df["Order Date"])
+
+st.write("### Veri Önizleme")
+st.dataframe(df.head())
